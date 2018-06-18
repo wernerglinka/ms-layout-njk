060dd62a9e6d1a4b1be5aaa54219e47afd40a18a
Werner Glinka <wernerglinka@Werners-MacBook-Pro.local>
2018-06-18 10:27:50 -0700

    changed index text to show pluigin versions

--- build.js
+++ build.js
@@ -1,43 +1,42 @@
-#!/usr/bin/env node
+/*jslint es6 */
 
 /*
  * Metalsmith build file
- * Build site with `node ./build.js` or `npm start`
+ * Build site with `node build`
  */
 
 'use strict';
 
 const toUpper = function (string) {
-	"use strict";
-	return string.toUpperCase();
+    return string.toUpperCase();
 };
 
 const spaceToDash = function (string) {
-	"use strict";
-	return string.replace(/\s+/g, "-");
+    return string.replace(/\s+/g, "-");
 };
 
-const consoleLog = true; 
 const inplace = require('metalsmith-in-place');
 const layouts = require('metalsmith-layouts');
 const metalsmith = require('metalsmith');
 
 const templateConfig = {
-		engineOptions: {
-			filters: {
-				toUpper: toUpper,
-				spaceToDash: spaceToDash
-			}
-		}
-	};
+    engineOptions: {
+        filters: {
+            toUpper: toUpper,
+            spaceToDash: spaceToDash
+        }
+    }
+};
 
 metalsmith(__dirname)
-	.clean(true)
-	.source('./src/')
-	.destination('./build/')
-	.use(inplace(templateConfig))
-	.use(layouts(templateConfig))
-	.build(function(err) {
-	if (err) throw err;
-		console.log('Build finished!');
-	});
+    .clean(true)
+    .source('./src/')
+    .destination('./build/')
+    .use(inplace(templateConfig))
+    .use(layouts(templateConfig))
+    .build(function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('Build finished!');
+    });
